c27cea6304789c367dd2f7d8edd45ccef0f3c4bc
Maël Querré <mael.querre@gmail.com>
2019-12-05 00:10:25 +0100

    Configure Eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,17 @@
-module.exports = {
-  dir: {
-    input: 'docs',
-    output: 'docs/_site',
-    layouts: '_layouts'
-  },
-  templateFormats: ['css']
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight);
+
+  eleventyConfig.addPassthroughCopy("css")
+  eleventyConfig.addPassthroughCopy("images")
+
+  return {
+    templateFormats: ['html'],
+    dir: {
+      input: 'docs',
+      output: 'docs/_site',
+      layouts: '_layouts'
+    }
+  }
 }
